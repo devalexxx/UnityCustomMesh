5b5f3b7b497306ce2b8fec540901816c70624167
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-09-27 11:47:35 +0200

    Add Cone

---
+++ Assets/Scripts/Cone.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cone : MonoBehaviour
+{
+    private MeshFilter filter;
+
+    [SerializeField]
+    private float radius = 1.0f;
+
+    [SerializeField]
+    private float height = 3.0f;
+
+    [SerializeField]
+    private uint nMeridians = 16;
+
+    void Awake()
+    {
+        filter = GetComponent<MeshFilter>();
+    }
+
+    void Start()
+    {
+        Mesh mesh = filter.mesh;
+
+        List<Vector3> vertices  = new();
+        List<int>     triangles = new();
+
+        vertices.Add(new Vector3(0.0f, 0.0f, 0.0f));
+        vertices.Add(new Vector3(0.0f, height, 0.0f));
+
+        int centerBotIndex = 0;
+        int centerTopIndex = 1;
+
+        // Body
+        {
+            float theta = 2 * Mathf.PI / nMeridians;
+
+            vertices.Add(new(
+                radius * Mathf.Cos(0.0f),
+                0.0f,
+                radius * Mathf.Sin(0.0f)
+            ));
+            int firstBotPointIndex = vertices.Count - 1;
+            int botPointIndex      = firstBotPointIndex;
+
+            for (int i = (int)(nMeridians - 1); i > 0; i--)
+            {
+                float angle = i * theta;
+
+                vertices.Add(new(
+                    radius * Mathf.Cos(angle),
+                    0.0f,
+                    radius * Mathf.Sin(angle)
+                ));
+
+                int nextBotPointIndex = vertices.Count - 1;
+
+                triangles.AddRange(new int[] {
+                    centerTopIndex, botPointIndex, nextBotPointIndex,
+                });
+
+                botPointIndex = nextBotPointIndex;
+            }
+
+            triangles.AddRange(new int[] {
+                centerTopIndex, botPointIndex, firstBotPointIndex,
+            });
+        }
+
+        // Bottom face
+        {
+            int startBotIndex   = 2;
+            int currentBotIndex = startBotIndex;
+            // From last element - 1 to 0
+            for (int i = vertices.Count - 1; i >= startBotIndex + 1; --i)
+            {
+                triangles.AddRange(new int[] { centerBotIndex, currentBotIndex, i });
+                currentBotIndex = i;
+            }
+            // Because it's a circle, first vertex is also the last
+            triangles.AddRange(new int[] { centerBotIndex, currentBotIndex, startBotIndex });
+        }
+
+        mesh.vertices  = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+    }
+}
